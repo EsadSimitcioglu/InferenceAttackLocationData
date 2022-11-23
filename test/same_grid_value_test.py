2e9a1bc56ad7b0f9faf2621f890b1ff0fb07eeee
EsadSimitcioglu <esad.simitcioglu@ozu.edu.tr>
2022-11-23 23:14:02 +0300

    Refactor

--- test/same_grid_value_test.py
+++ test/same_grid_value_test.py
@@ -1,27 +1,30 @@
 # Common libraries
 import numpy as np
+from numpy import log as ln
 import matplotlib.pyplot as plt
 
 # Multi-Freq-LDPy functions for L-SUE protocol (a.k.a. Basic RAPPOR[11])
 from LDP.protocols import GRR_Client, GRR_Aggregator, RAPPOR_Client, RAPPOR_Aggregator
 
 
-def grr_estimated_freq_per_epsilon(grid_list, k, epsilon):
+def grr_estimated_freq_per_epsilon(gird_number, grid_list, k, epsilon):
     grr_reports = [GRR_Client(user_val, k, epsilon) for user_val in grid_list]
     grr_est_freq = GRR_Aggregator(grr_reports, k, epsilon)
-    return grr_est_freq[gird_number + 1]
+    probability = grr_est_freq[gird_number + 1] / sum(grr_est_freq)
+    return probability
 
 
-def rappor_estimated_freq_per_epsilon(grid_list, k, epsilon, epsilon_l):
+def rappor_estimated_freq_per_epsilon(gird_number, grid_list, k, epsilon, epsilon_l):
     rappor_reports = RAPPOR_Client(grid_list, k, epsilon, epsilon_l)
     rappor_est_freq = RAPPOR_Aggregator(rappor_reports[0], epsilon, epsilon_l)
-    return rappor_est_freq[gird_number + 1]
+    probability = rappor_est_freq[gird_number + 1] / sum(rappor_est_freq)
+    return probability
 
 
 # Parameters for simulation
 n = 1  # number of users
 k = 20  # attribute's domain size (grid size)
-epsilon_list = [1, 2, 4, 8, 20, 50]  # number of epsilon for test cases
+epsilon_list = [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4]  # number of epsilon for test cases
 gird_number = 3  # user's grid number (location)
 
 grr_estimated_fre_per_epsilon = list()
@@ -37,18 +40,17 @@ data = np.random.randint(k, size=n)
 for epsilon in epsilon_list:
     epsilon_l = 0.5 * epsilon  # single report privacy guarantee, i.e., lower bound
 
-    grr_est_freq = grr_estimated_freq_per_epsilon(grid_list, k, epsilon)
+    grr_est_freq = grr_estimated_freq_per_epsilon(gird_number, grid_list, k, epsilon)
     grr_estimated_fre_per_epsilon.append(grr_est_freq)
 
-    rappor_est_freq = rappor_estimated_freq_per_epsilon(grid_list, k, epsilon, epsilon_l)
+    rappor_est_freq = rappor_estimated_freq_per_epsilon(gird_number, grid_list, k, epsilon, epsilon_l)
     rappor_estimated_fre_per_epsilon.append(rappor_est_freq)
-
 
 plt.ylim(0, 1)
 plt.xlim(min(epsilon_list), max(epsilon_list))
-plt.plot(epsilon_list, grr_estimated_fre_per_epsilon, label='GRR Estimated Frequency', color='red')
-plt.plot(epsilon_list, rappor_estimated_fre_per_epsilon, label='RAPPOR Estimated Frequency', color='blue')
-plt.ylabel('Estimated Frequency')
+plt.plot(epsilon_list, grr_estimated_fre_per_epsilon, label='GRR', color='red')
+plt.plot(epsilon_list, rappor_estimated_fre_per_epsilon, label='RAPPOR', color='blue')
+plt.ylabel('Probability of Guess')
 plt.xlabel('Epsilon values')
 plt.legend(loc='upper right', bbox_to_anchor=(1.015, 1.15))
 plt.show()
