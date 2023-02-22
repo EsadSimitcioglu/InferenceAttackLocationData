76eb3deb7dc470ed010e6569a5e094a01aafbbb7
EsadSimitcioglu <esad.simitcioglu@ozu.edu.tr>
2023-02-22 23:46:05 +0300

    Add trained hmm test

--- LDP/protocols_estimation_different_grid.py
+++ LDP/protocols_estimation_different_grid.py
@@ -1,5 +1,5 @@
 import numpy as np
-from hidden_markov_model import hmm_model_GRR
+from hidden_markov_model import hmm_model_GRR, hmm_model_GRR_pre_analyze
 from LDP.protocols import GRR_Client, SIMPLE_RAPPOR_Client
 
 states = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
@@ -7,9 +7,13 @@ observations = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "
                 "20"]
 
 
-def grr_estimated_guess(user_values_list, k, epsilon):
-    model = hmm_model_GRR(epsilon, k)
+def grr_estimated_guess(user_values_list, k, epsilon, model_type):
+    if model_type == "plain":
+        model = hmm_model_GRR(epsilon, k)
+    else:
+        model = hmm_model_GRR_pre_analyze(epsilon, k, user_values_list)
     epsilon_prob = list()
+
     for user_true_values in user_values_list:
         grr_reports = [GRR_Client(user_true_value, k, epsilon) for user_true_value in user_true_values]
 
--- hidden_markov_model.py
+++ hidden_markov_model.py
@@ -89,12 +89,62 @@ def hmm_model_GRR(epsilon, k):
     return discrete_model
 
 
+def hmm_model_GRR_pre_analyze(epsilon, k, user_value_list):
+    p = np.exp(epsilon) / (np.exp(epsilon) + k - 1)
+    q = (1 - p) / (k - 1)
+
+    discrete_model = hmm.MultinomialHMM(n_components=k,
+                                        algorithm='viterbi',  # decoder algorithm.
+                                        )
+
+    start_grid_dict = {}
+    for user_true_values in user_value_list:
+        init_grid = user_true_values.item(0)
+        start_grid_dict[init_grid] = start_grid_dict.get(init_grid, 0) + 1
+
+    start_grid_dict = dict(sorted(start_grid_dict.items()))
+    sum_grid_counts = sum(start_grid_dict.values())
+    start_prob_list = list()
+    for grid_count in start_grid_dict.values():
+        start_prob_list.append(grid_count / sum_grid_counts)
+
+    discrete_model.startprob_ = np.array(start_prob_list)
+
+    adjacent_matrix = np.arange(20).reshape(5, 4)
+    matrix_list = []
+    for i in range(1, k + 1):
+        sub_list = []
+        adjacent_elements = getAdjacent(adjacent_matrix, i)
+        for j in range(1, k + 1):
+            if j in adjacent_elements or j == i:
+                sub_list.append(1 / (len(adjacent_elements) + 1))
+            else:
+                sub_list.append(0)
+        matrix_list.append(sub_list)
+
+    discrete_model.transmat_ = np.array(matrix_list)
+
+    matrix_list = []
+    for i in range(k):
+        row_list = []
+        for j in range(k):
+            if i == j:
+                row_list.append(p)
+            else:
+                row_list.append(q)
+        matrix_list.append(row_list)
+
+    discrete_model.emissionprob_ = np.array(matrix_list)
+
+    return discrete_model
+
+
 adjacent_matrix = np.arange(20).reshape(5, 4)
 matrix_list = []
-for i in range(1,21):
+for i in range(1, 21):
     sub_list = []
     adjacent_elements = getAdjacent(adjacent_matrix, i)
-    for j in range(1,21):
+    for j in range(1, 21):
         if (j) in adjacent_elements or j == i:
             sub_list.append(1 / (len(adjacent_elements) + 1))
         else:
