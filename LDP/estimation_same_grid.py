0469a3942cec87e47a822cdd5ceb7c98e4a16b52
EsadSimitcioglu <esadsimitcioglu@gmail.com>
2023-07-29 01:50:06 +0300

    Debug and add plot_grid

--- LDP/estimation_same_grid.py
+++ LDP/estimation_same_grid.py
@@ -2,13 +2,29 @@ from statistics import mode
 import random
 import numpy as np
 import xxhash
+from collections import Counter
+
 
 from LDP.protocols import OUE_Client, OLH_Client, SIMPLE_RAPPOR_Client, GRR_Client, OLH_Client2
+
+
+def find_mode(perturbed_reports):
+    data = Counter(perturbed_reports)
+    get_mode = dict(data)
+    mode = [k for k, v in get_mode.items() if v == max(list(data.values()))]
+    if len(mode) > 1:
+        reports_mode = random.choice(mode)
+    else:
+        reports_mode = mode[0]
+
+    return reports_mode
 
 
 def grr_estimated_guess_per_user(user_values_list, k, epsilon):
     grr_reports = [GRR_Client(user_true_value, k, epsilon) for user_true_value in user_values_list]
     grr_reports_mode = mode(grr_reports)
+    if grr_reports.count(grr_reports_mode) > 1:
+        grr_reports_mode = random.choice(grr_reports)
     return grr_reports_mode
 
 
@@ -17,7 +33,7 @@ def grr_estimated_guess(user_values_list, k, epsilon):
     for user_true_values in user_values_list:
         grid_number = user_true_values[0] - 1
         grr_reports = [GRR_Client(user_true_value, k, epsilon) for user_true_value in user_true_values]
-        grr_reports_mode = mode(grr_reports)
+        grr_reports_mode = find_mode(grr_reports)
         if grr_reports_mode == grid_number:
             probability_per_user.append(1)
         else:
@@ -61,21 +77,23 @@ def olh_estimated_guess(user_values_list, k, epsilon):
     seed_init = 0
     g = int(round(np.exp(epsilon))) + 1
 
+
+
     for user_true_values in user_values_list:
         true_value = user_true_values[0]
-        hash_true_value = (xxhash.xxh32(str(true_value - 1), seed=seed_init).intdigest() % g)
         olh_reports = OLH_Client2(user_true_values, k, epsilon, seed_init)
-        olh_reports_mode = mode(olh_reports)
+
+        olh_reports_mode = find_mode(olh_reports)
 
         grid_list = list()
         for grid_number in range(1, k + 1):
             grid_number_power = np.repeat(grid_number, 100)
             olh_guess_reports = OLH_Client2(grid_number_power, k, epsilon, seed_init)
-            olh_guess_mode = mode(olh_guess_reports)
-            if hash_true_value == olh_guess_mode:
+            olh_guess_mode = find_mode(olh_guess_reports)
+            if olh_reports_mode == olh_guess_mode:
                 grid_list.append(grid_number)
 
-        if olh_reports_mode == hash_true_value:
+        if len(grid_list) > 1:
             random_grid = random.choice(grid_list)
             if random_grid == true_value:
                 probability_per_user.append(1)
---
+++ plot_grid.py
@@ -0,0 +1,58 @@
+import matplotlib.pyplot as plt
+from shapely import geometry, ops
+from shapely.geometry import Polygon, polygon
+from PIL import Image
+
+max_lat = 40.05
+max_long = 116.55
+min_lat = 39.8
+min_long = 116.2
+
+
+##construct the rectangle using shapely
+rec = [(min_lat, min_long), (min_lat, max_long), (max_lat, max_long), (max_lat, min_long)]
+nx, ny = 9, 9
+
+polygon = geometry.Polygon(rec)
+minx, miny, maxx, maxy = polygon.bounds
+dx = (maxx - minx) / nx
+dy = (maxy - miny) / ny
+horizontal_splitters = [geometry.LineString([(minx, miny + i * dy), (maxx, miny + i * dy)]) for i in range(ny)]
+vertical_splitters = [geometry.LineString([(minx + i * dx, miny), (minx + i * dx, maxy)]) for i in range(nx)]
+splitters = horizontal_splitters + vertical_splitters
+
+result = polygon
+for splitter in splitters:
+    result = geometry.MultiPolygon(ops.split(result, splitter))
+
+grids = list(result.geoms)
+
+
+# Load the map image (replace 'map_image.jpg' with the actual path to your map image)
+map_image = Image.open('ss.png')
+
+# Create a new figure
+fig, ax = plt.subplots()
+
+# Plot the map image as the background for the entire plot
+ax.imshow(map_image, extent=[minx, maxx, miny, maxy], aspect='auto')
+
+# Your existing code to plot polygons and grids
+x, y = polygon.exterior.xy
+plt.plot(x, y, color='#6699cc', alpha=0.7, linewidth=3, solid_capstyle='round', zorder=2)
+
+for grid in grids:
+    x, y = grid.exterior.xy
+    plt.plot(x, y, color='#6699cc', alpha=0.7, linewidth=3, solid_capstyle='round', zorder=2)
+
+# Remove axis labels and ticks
+ax.set_xticks([])
+ax.set_yticks([])
+
+# Remove grid lines
+ax.grid(False)
+
+# Show the plot
+plt.show()
+
+
