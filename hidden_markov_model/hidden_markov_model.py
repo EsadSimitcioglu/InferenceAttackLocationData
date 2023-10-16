3abd7ae70511fcaf9ec460f0d7b014387ce1b8ec
EsadSimitcioglu <esadsimitcioglu@gmail.com>
2023-10-16 23:48:01 +0300

    Add OLH_bit_vector model

--- hidden_markov_model/hidden_markov_model.py
+++ hidden_markov_model/hidden_markov_model.py
@@ -255,7 +255,6 @@ def hmm_model_RAPPOR(epsilon, k, train_type, user_value_list=None):
             row_prob_list.append(prob)
         emission_prob_list.append(row_prob_list)
 
-
     model.emissionprob_ = np.array(emission_prob_list)
 
     return model
@@ -453,5 +452,119 @@ def hmm_model_OLH(epsilon, k, seed_counter, train_type, user_value_list=None):
         matrix_list.append(row_list)
 
     model.emissionprob_ = np.array(matrix_list)
+
+    return model
+
+def hmm_model_ESAD(epsilon, k, train_type, user_value_list=None):
+    p = np.exp(epsilon / 2) / (np.exp(epsilon / 2) + 1)
+    q = 1 / (np.exp(epsilon / 2) + 1)
+
+    g = int(round(np.exp(epsilon))) + 1
+
+
+    model = hmm.MultinomialHMM(n_components=k, algorithm='viterbi')
+
+    if train_type == 'plain':
+
+        model.startprob_ = np.array([1 / k] * k)
+
+        adjacent_matrix = np.arange(k).reshape(5, 4)
+        transmat_prob_list = []
+        for i in range(1, k + 1):
+            sub_list = []
+            adjacent_elements = getAdjacent(adjacent_matrix, i)
+            for j in range(1, k + 1):
+                if j in adjacent_elements or j == i:
+                    sub_list.append(1 / (len(adjacent_elements) + 1))
+                else:
+                    sub_list.append(0)
+            transmat_prob_list.append(sub_list)
+
+        model.transmat_ = np.array(transmat_prob_list)
+
+    elif train_type == 'advance':
+        dict_of_path = analyze_taken_path(user_value_list)
+
+        start_grid_dict = {}
+        for user_true_values in user_value_list:
+            init_grid = user_true_values.item(0)
+            start_grid_dict[init_grid] = start_grid_dict.get(init_grid, 0) + 1
+
+        start_grid_dict = dict(sorted(start_grid_dict.items()))
+        sum_grid_counts = sum(start_grid_dict.values())
+        start_prob_list = list()
+        for grid_count in start_grid_dict.values():
+            start_prob_list.append(grid_count / sum_grid_counts)
+
+        for _ in range(k - len(start_prob_list)):
+            start_prob_list.append(0)
+
+        model.startprob_ = np.array(start_prob_list)
+
+        adjacent_matrix = np.arange(20).reshape(5, 4)
+        matrix_list = []
+        for i in range(1, k + 1):
+            sub_list = []
+            adjacent_elements = getAdjacent(adjacent_matrix, i)
+            sum_of_path = 0
+            if i in dict_of_path and i in dict_of_path[i]:
+                sum_of_path = dict_of_path[i][i]
+            else:
+                if i in dict_of_path:
+                    dict_of_path[i][i] = 1
+                else:
+                    dict_of_path[i] = {}
+                    dict_of_path[i][i] = 1
+                sum_of_path += 1
+            for element in adjacent_elements:
+                if element in dict_of_path[i]:
+                    sum_of_path += dict_of_path[i][element]
+                else:
+                    dict_of_path[i][element] = 1
+                    sum_of_path += 1
+            for j in range(1, k + 1):
+                if (j in adjacent_elements or j == i) and (j in dict_of_path[i]):
+                    taken_path = dict_of_path[i][j]
+                    sub_list.append(taken_path / sum_of_path)
+                else:
+                    sub_list.append(0)
+            matrix_list.append(sub_list)
+
+        model.transmat_ = np.array(matrix_list)
+
+    rappor_report_list = list()
+    for x in range(2 ** g):
+        rappor_report_list.append((bin(x)[2:].zfill(g)))
+
+    user_value_list = list()
+    for i in range(g):
+        bit_vector = ''
+        for j in range(g):
+            if i == j:
+                bit_vector += '1'
+            else:
+                bit_vector += '0'
+        user_value_list.append(bit_vector)
+
+    emission_prob_list = list()
+    p_counter = 0
+    q_counter = 0
+    for row_index in range(len(user_value_list)):
+        row = user_value_list[row_index]
+        row_prob_list = list()
+        for column_index in range(len(rappor_report_list)):
+            column = rappor_report_list[column_index]
+            prob = 1
+            for char_index in range(len(row)):
+                if row[char_index] == column[char_index]:
+                    p_counter += 1
+                    prob *= p
+                else:
+                    q_counter += 1
+                    prob *= q
+            row_prob_list.append(prob)
+        emission_prob_list.append(row_prob_list)
+
+    model.emissionprob_ = np.array(emission_prob_list)
 
     return model
