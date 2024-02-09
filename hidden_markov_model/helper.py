a8f659654803cf5331f4fd03f369598f21e3ee01
EsadSimitcioglu <esadsimitcioglu@gmail.com>
2024-02-09 23:16:21 +0300

    Add one state implementation to helper file

--- hidden_markov_model/helper.py
+++ hidden_markov_model/helper.py
@@ -1,3 +1,8 @@
+from collections import defaultdict
+
+import numpy as np
+
+
 def isValidPos(i, j, n, m):
     if i < 0 or j < 0 or i > n - 1 or j > m - 1:
         return 0
@@ -110,3 +115,86 @@ def decimal_to_binary(decimal_num, k):
         padding = '0' * (k - len(binary_num))
         binary_num = padding + binary_num
     return binary_num
+
+
+def create_rappor_emission_matrix(self, rappor, seed):
+    row_value_list = create_emission_matrix_rows(self.k)
+
+    column_value_list = create_emission_matrix_column(self.k)
+
+    keep_bit_prob = ((rappor.p ** 2) + (rappor.q ** 2)) if rappor.is_memoized else rappor.p
+    flip_bit_prob = (2 * rappor.p * rappor.q) if rappor.is_memoized else rappor.q
+
+    order = 0
+    emission_prob_list = list()
+    for row_index in range(len(column_value_list)):
+        row = column_value_list[row_index]
+        row_prob_list = list()
+        for column_index in range(len(row_value_list)):
+            column = row_value_list[column_index]
+            p_counter = 0
+            q_counter = 0
+            for char_index in range(len(row)):
+                if row[char_index] == column[char_index]:
+                    p_counter += 1
+                else:
+                    q_counter += 1
+
+            row_prob_list.append((order, p_counter, q_counter))
+            order += 1
+        emission_prob_list.append(row_prob_list)
+
+    order = -1
+    order_prime = 0
+    revised_column_dict_order = defaultdict(lambda: -1)
+    q_counter_to_value_dict = defaultdict(lambda: -1)
+
+    for column_index in range(len(emission_prob_list[0])):
+        q_counter = 0
+        for row_index in range(len(emission_prob_list)):
+            element = emission_prob_list[row_index][column_index]
+            q_counter += element[2]
+
+        if q_counter <= (self.k // 4) * self.k:
+            order += 1
+            revised_column_dict_order[row_value_list[column_index]] = order
+            q_counter_to_value_dict[row_value_list[column_index]] = order
+        else:
+            if order_prime == 0:
+                order_prime = order + 1
+                order += 1
+                revised_column_dict_order[order_prime] = []
+            revised_column_dict_order[order_prime].append(row_value_list[column_index])
+            q_counter_to_value_dict[row_value_list[column_index]] = order_prime
+
+    emission_prob_list = list()
+    for row_index in range(len(column_value_list)):
+        row = column_value_list[row_index]
+        row_prob_list = list()
+
+        prob_list = list()
+        for column in revised_column_dict_order:
+            prob = 1
+
+            if column == order_prime:
+                final_prob = 0
+                for column_index in range(len(revised_column_dict_order[column])):
+                    prob = 1
+                    for char_index in range(len(row)):
+                        if row[char_index] == revised_column_dict_order[column][column_index][char_index]:
+                            prob *= keep_bit_prob
+                        else:
+                            prob *= flip_bit_prob
+                    final_prob += prob
+                row_prob_list.append(final_prob)
+            else:
+                for char_index in range(len(row)):
+                    if row[char_index] == column[char_index]:
+                        prob *= keep_bit_prob
+                    else:
+                        prob *= flip_bit_prob
+                row_prob_list.append(prob)
+        emission_prob_list.append(row_prob_list)
+
+    self.dict_order = q_counter_to_value_dict
+    self.model.emissionprob_ = np.array(emission_prob_list)
